a3298729529442f28fc81e95f387c69c11ff5b12
Artur <arthur.gaspar2011@gmail.com>
2023-06-22 18:47:36 -0300

    Fixed corner case.

--- k_centers.py
+++ k_centers.py
@@ -5,6 +5,9 @@ from utils import mink_dist, dist_matrix
 def k_centers(points, k, dists):
 	n = points.shape[0]
 
+	if k >= n:
+		return np.array(range(points.shape[0]))
+
 	ans = np.zeros(k,dtype=int)
 	# First point is chosen randomly:
 	ans[0] = np.random.randint(0,points.shape[0])
@@ -21,14 +24,14 @@ def k_centers(points, k, dists):
 		for j in range(n):
 			dist_from_ans[j] = min(dist_from_ans[j],dists[j][selected])
 		ans[i] = selected
-	return ans
+	return points[ans]
 
+# The code below only runs when this file is not used as a module. This should be used only for debugging, the experiments are implemented in another file.
 if __name__ == "__main__":
 	np.random.seed(123456)
 	n = int(input("Type the number of the data points: "))
 	dim = int(input("Type the dimension of the data points: "))
 
-	# TODO: Change this input format!
 	print("Now inform all the points (one point per line, dimensions space-separated): ")
 	points = np.zeros((n,dim),dtype=float)
 	for i in range(n):
@@ -44,8 +47,5 @@ if __name__ == "__main__":
 
 	n_iter = int(input("Type the number of iterations you want to run: "))
 	for i in range(n_iter):
-		print("Indices dos pontos selecionados: ")
-		ans = k_centers(points, k, dist_matr)
-		print(ans)
-		print("Pontos selecionados:")
-		print(points[ans])
+		print("Pontos selecionados: ")
+		print(k_centers(points, k, dist_matr))
